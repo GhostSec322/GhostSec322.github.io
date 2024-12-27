c26196fe05a13a6296be25ad038bfe2e580662ea
GhostSec322 <GhostSec322@gmail.com>
2024-12-27 16:29:50 +0900

    feat: security

--- _posts/output/hello.c
+++ _posts/output/hello.c
@@ -1,8 +1,24 @@
-#include<stdio.h>
-void hello(name){
-  printf("Hello %s",name);
+#include <stdio.h>
+
+int swapfun(int *a, int *b) {
+    int c = *a;  // `a`가 가리키는 값을 저장
+    printf("c is ...%d\n", c);
+
+    *a = *b;  // `a`가 가리키는 값을 `b`가 가리키는 값으로 변경
+    printf("a is ...%d\n", *a);
+
+    *b = c;  // `b`가 가리키는 값을 `c`로 변경
+    printf("b is ...%d\n", *b);
+
+    return 0;
 }
-int main(void){
-  hello("GhostSec");
-  return 0;
+
+int main(void) {
+    int a1 = 0;
+    int b2 = 3;
+
+    swapfun(&a1, &b2);  // 주소를 전달하여 값 교환
+    printf("%d %d\n", a1, b2);  // 결과 출력
+
+    return 0;
 }
--- _posts/output/ㅊ.c
+++ _posts/output/ㅊ.c
@@ -1,19 +1,10 @@
-#include<stdio.h>
-int check(int x){
-    int count =0;
-    for( int i=1;i<=x;i++){
-        if(x%i==0){
-            count++;
-        }
-    }
-    if (count==2){
-        printf("소수");
-    }
-    else{
-        printf("소수아님");
-    }
-}
-int main(void){
-    check(11);
-    return 0;
+#include <stdio.h>
+int main(void) {
+int a[5] = { 1, 2, 3, 4, 5 };
+int *p = a;
+printf("%d\n", *(p++));
+printf("%d\n", *(++p));
+printf("%d\n", *(p + 2));
+
+return 0;
 }
